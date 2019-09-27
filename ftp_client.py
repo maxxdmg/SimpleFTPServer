de0ad7c8749f3072f826092eaf8beaf77554efb4
maxxdmg <toorm@mail.gvsu.edu>
2019-09-27 10:50:06 -0400

    Added command reader modularity

--- ftp_client.py
+++ ftp_client.py
@@ -23,43 +23,57 @@ def main():
     #   sys.exit()
 
     while True:
-        print("Enter your command: QUIT to exit")
-        #command to go to the server
-        cmd = input()
-        #process commands
-        readcmd(cmd, sock)
+        print("Enter your command: HELP to see options or QUIT to exit")
+        cmd = input() #command to go to the server
+        readcmd(cmd, sock) #process commands
 
-def readcmd(rcmd, sock):
-    cmd = rcmd #.upper()
-
-    if cmd[0:5] == 'QUIT' or cmd[0:5] == 'quit':
+def handle_quit():
         sock.sendall(cmd.encode("UTF-8")) #send quit to server
         time.sleep(1) #added because close operation timing issues
         sock.close() #close socket
         sys.exit() #exit 
-            
-    if cmd[0:8] == 'RETRIEVE' or cmd[0:8] == 'retrieve':
-        rfile = cmd[9:] #parse file name
-        sock.sendall(cmd.encode("UTF-8"))
+
+def handle_retrieve():
+    rfile = cmd[9:] #parse file name
+    sock.sendall(cmd.encode("UTF-8"))
+    data = sock.recv(1024)
+    f = open(rfile, "w")
+    f.write(data.decode())
+    f.flush()
+    while True:
         data = sock.recv(1024)
-        f = open(rfile, "w")
+        if not data: break
+        print('data=%s', (data))
         f.write(data.decode())
         f.flush()
-        while True:
-            data = sock.recv(1024)
-            if not data: break
-            print('data=%s', (data))
-            f.write(data.decode())
-            f.flush()
 
-            #if less than 1024 nothing more to receive
-            if len(data) < 1024:
-                f.close()
-                break
-        f.close()
-        print('Successfully received the file')
+        #if less than 1024 nothing more to receive
+        if len(data) < 1024:
+            f.close()
+            break
+    f.close()
+    print('Successfully received the file')
 
-    if cmd[0:10] == 'CONNECTION':
+def handle_help():
+    print("QUIT: to quit\nRETRIEVE: to retrieve files\n")
+
+def readcmd(rcmd, sock):
+    cmd = rcmd.lower() #.upper()
+
+    # handle help
+    if 'help' in cmd:
+        handle_help()
+
+    # handle quit
+    if 'quit' in cmd:
+        handle_quit()
+    
+    # handle retrieve
+    if 'retreieve' in cmd:
+        handle_retrieve() 
+
+    # handle connection
+    if 'connection' in cmd:
         cmdlist = cmd.split(" ", 3)
         ip = cmdlist[1]
         port = cmdlist[2]
